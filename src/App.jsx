daf3f3714ed740af85f6cdc1b20ddac2aa875955
tangkhanhnhi111 <tangkhanhnhi111@gmail.com>
2025-12-02 20:10:03 +0700

    feat: faq page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
+
+
 import CollectionsPage from "./pages/collections/CollectionsPage";
 import CollectionDetailPage from "./pages/collections/CollectionDetailPage";
-import NewsListPage from './pages/news/NewsListPage'
-import NewsDetailPage from './pages/news/NewsDetailPage'
+import ServicesPage from "./pages/services/ServicesPage";
+import NewsListPage from "./pages/news/NewsListPage";
+import NewsDetailPage from "./pages/news/NewsDetailPage";
+import FAQPage from "./pages/faq/FAQPage";
 import PrivacyPolicyPage from "./pages/policies/PrivacyPolicyPage";
 import WarrantyPolicyPage from "./pages/policies/WarrantyPolicyPage";
 import ShippingReturnPolicyPage from "./pages/policies/ShippingReturnPolicyPage";
-import ServicesPage from './pages/services/ServicesPage';
-import StoreLocatorPage from './pages/stores/StoreLocatorPage';
+import StoreLocatorPage from "./pages/stores/StoreLocatorPage";
 
-const App = () => {
+
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
-          {/* <Route path="/" element={<HomePage />} /> */}
 
 
           <Route path="/collections" element={<CollectionsPage />} />
           <Route path="/collections/:id" element={<CollectionDetailPage />} />
 
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/services" element={<ServicesPage />} />
+
           <Route path="/news" element={<NewsListPage />} />
           <Route path="/news/:slug" element={<NewsDetailPage />} />
+
+          <Route path="/faq" element={<FAQPage />} />
 
           <Route path="/policies/privacy" element={<PrivacyPolicyPage />} />
           <Route path="/policies/warranty" element={<WarrantyPolicyPage />} />
@@ -32,13 +40,11 @@ const App = () => {
             element={<ShippingReturnPolicyPage />}
           />
 
-          <Route path="/services" element={<ServicesPage />} />
           <Route path="/stores" element={<StoreLocatorPage />} />
+
 
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
-
-export default App
